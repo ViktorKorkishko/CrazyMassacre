6c24a3cc1e603fc0f980c109ef28c4ae1ba4e21f
Viktor Korkishko <korkishko.viktor2001@gmail.com>
2022-11-12 14:08:04 +0200

    [fixed] PhotonView binding

--- CrazyMassacreUnityProject/Assets/Scripts/Game/Player/Movement/PlayerMovementInstaller.cs
+++ CrazyMassacreUnityProject/Assets/Scripts/Game/Player/Movement/PlayerMovementInstaller.cs
@@ -1,5 +1,6 @@
 using Game.Player.Movement.Controllers;
 using Game.Player.Movement.Models;
+using Photon.Pun;
 using UnityEngine;
 using Zenject;
 
@@ -7,12 +8,14 @@ namespace Game.Player.Movement.Installers
 {
     public class PlayerMovementInstaller : MonoInstaller
     {
+        [SerializeField] private PhotonView _photonView;
         [SerializeField] private Rigidbody2D _rigidbody;
 
         public override void InstallBindings()
         {
             Container.Bind<Rigidbody2D>().FromComponentOn(_rigidbody.gameObject).AsSingle();
-
+            Container.Bind<PhotonView>().FromComponentOn(_photonView.gameObject).AsSingle();
+            
             Container.Bind<PlayerMovementModel>().AsSingle().NonLazy();
             Container.BindInterfacesAndSelfTo<PlayerMovementController>().AsSingle();
         }
